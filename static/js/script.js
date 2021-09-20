d97d2f40ec1248a356e936cbe80b682831fe3cb7
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2021-09-20 10:35:22 +0600

    init

--- static/js/script.js
+++ static/js/script.js
@@ -4,24 +4,25 @@ const header_toggle = document.querySelector("#header_toggle");
 const header = document.querySelector(".header");
 const body = document.querySelector("body");
 //const overlay = document.querySelector(".overlay");
-const fadeElement =document.querySelectorAll(".has-fade");
+const fadeElement = document.querySelectorAll(".has-fade");
 
 header_toggle.addEventListener("click", function () {
-  
+
   //console.log("clicked");
 
   if (header.classList.contains("open")) {
     header.classList.remove("open");
     body.classList.remove("noscroll");
-    fadeElement.forEach(function(element){
-      element.classList.add("fade-out");
+    fadeElement.forEach(function (element) {
       element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+
     });
-    
+
   } else {
     header.classList.add("open");
     body.classList.add("noscroll");
-    fadeElement.forEach(function(element){
+    fadeElement.forEach(function (element) {
       element.classList.add("fade-in");
       element.classList.remove("fade-out");
     });
