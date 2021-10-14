03c693075ab6fb568123b9153dccda66b123e669
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2021-10-15 03:43:32 +0600

    init

--- nasa_api.py
+++ nasa_api.py
@@ -1,17 +1,48 @@
+import urllib.request
 import requests
 import json
 import os
 from PIL import Image
+#import datetime
+from datetime import datetime, timedelta,date
 
 
 def get_data(api_key):
+    # raw_response = requests.get(
+    #     f'https://api.nasa.gov/planetary/apod?api_key={api_key}').text
+    # response = json.loads(raw_response)
+    #d = datetime.date.today()
+    #da = datetime.datetime.now()
+    #print(" Todays date ", d)
+
+    # taking input as the date
+    Begindatestring = datetime.date.today()
+
+# carry out conversion between string
+# to datetime object
+    #Begindate = datetime.strptime(Begindatestring, "%Y-%m-%d")
+
+
+# print begin date
+    print("Beginning date", Begindatestring)
+    #print(Begindate)
+
+
+# calculating end date by adding 10 days
+    #Enddate = Begindate + timedelta(days=10)
+
+# printing end date
+    print("Ending date")
+    #print(Enddate)
     raw_response = requests.get(
-        f'https://api.nasa.gov/planetary/apod?api_key={api_key}').text
+        f'https://api.nasa.gov/planetary/apod?api_key={api_key}&date=1995-06-16').text
     response = json.loads(raw_response)
+
     return response
 
 
 def get_date(response):
+
     date = response['date']
     return date
 
@@ -66,5 +97,60 @@ def convert_image(image_path):
     base_directory = os.path.dirname(path_to_image)
 
     image = Image.open(path_to_image)
-    #image.save(f"{base_directory}/{filename_no_extension}.png")
+    # image.save(f"{base_directory}/{filename_no_extension}.png")
     image.save(f"static/image/{filename_no_extension}.png")
+
+
+# #import pandas as pd
+# #from IPython.display import Image, display
+
+# #Initialize Nasa class by creating an object:
+# k = "523p5hPYHGzafYGLCkqa54kKMTV2vbP0XcPxkcLm"
+# nasa = nasapy.Nasa(key=k)
+
+# #Get a list of dates:
+# dates = pd.date_range(end='2020-08-15', periods=5)
+
+# #Empty list to store dictionary keys and values:
+# data = []
+
+# #Getting all the data:
+# for d in dates:
+#     apod = nasa.picture_of_the_day(d, hd=True)
+
+#     if apod['media_type'] == 'image':
+#         if 'hdurl' in apod.keys():
+#             data.append(
+#                 {'date': apod['date'], 'title': apod['title'], 'hdurl': apod['hdurl']})
+
+
+# #Path of the directory:
+# image_dir = "AAA_Images_1"
+
+# #Checking if the directory already exists?
+# dir_res = os.path.exists(image_dir)
+
+# #If it doesn't exist then make a new directory:
+# if (dir_res == False):
+#     os.makedirs(image_dir)
+
+# #If it exist then print a statement:
+# else:
+#     print("Directory already exists!\n")
+
+
+# #Retrieving the image:
+# for img in data:
+
+#     #Creating title for image:
+#     title = img["date"]+"_" + \
+#         img["title"].replace(" ", "_").replace(":", "_")+".jpg"
+
+#     #Downloading the image:
+#     urllib.request.urlretrieve(img['hdurl'], os.path.join(image_dir, title))
+
+# #Get list of images:
+# astro_images = os.listdir(image_dir)
+
+# #Displaying an image:
+# Image(os.path.join(image_dir, astro_images[0]))
