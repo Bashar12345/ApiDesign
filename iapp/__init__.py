25e97087c31803b2012fdf9abd163ff069de8600
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-01-14 23:30:12 +0600

    init

--- iapp/__init__.py
+++ iapp/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__) 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 
