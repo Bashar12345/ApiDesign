d37aaf9aafc9e339f6d88f56bd49d94af73ead5d
Bashar12345 <abul35-2002@diu.edu.bd>
2022-04-22 10:50:19 +0600

    init

--- backend/RED/views.py
+++ backend/RED/views.py
@@ -2,43 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from django.views.generic import ListView,DetailView
-from datetime import datetime, timedelta, date
-from RED import nasa_api
-import json
+from . import utils
 
 #from django.views import View
 #from django.contrib.auth.decorators import login_required
 
-NasaApI_KEY='KiEVTEhfZhZamuaQ3Hj2TjjHYYAkASrDbgxXT9f0'
+
 
 # Create your views here.
 
 def index(request):
     title = "RED-index"
-    # KiEVTEhfZhZamuaQ3Hj2TjjHYYAkASrDbgxXT9f0
-    today = date.today()
-    #today = today - timedelta(days=1)
-    previous_date = today - timedelta(days=1)
-    print(today)
-    print(previous_date)
-
-    nasa_api_response = nasa_api.get_data(NasaApI_KEY, today)
-    print("todays --",type(nasa_api_response))
-
-    previous_nasa_api_response = nasa_api.get_data(NasaApI_KEY, previous_date)
-    print("\nprevious days --",previous_nasa_api_response['url'])
-    jsondata = json.dumps(previous_nasa_api_response)
-    print(jsondata)
-
-
-    im_url = nasa_api.get_url(nasa_api_response)
-    im_url_previous = nasa_api.get_url(previous_nasa_api_response)
-
-    present_date = nasa_api.get_date(nasa_api_response)
-    yesterday = nasa_api.get_date(previous_nasa_api_response)
-
-    nasa_api.download_image(im_url, present_date)
-    nasa_api.download_image(im_url_previous, yesterday)
+    utils_of_index_page()
     return render(request,'RED/index.html', {'title': title,'present_date':present_date, 'yesterday':yesterday, 'nasa_api':nasa_api ,
         'previous_nasa_api_response':previous_nasa_api_response,'nasa_api_response':jsondata})
 
