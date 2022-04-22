461608fcf52d58224d3f44a3025d02606fa3bd1f
Bashar12345 <abul35-2002@diu.edu.bd>
2022-04-22 10:43:06 +0600

    init

--- backend/RED/views.py
+++ backend/RED/views.py
@@ -2,31 +2,92 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from django.views.generic import ListView,DetailView
+from datetime import datetime, timedelta, date
+from RED import nasa_api
+import json
+
 #from django.views import View
 #from django.contrib.auth.decorators import login_required
 
+NasaApI_KEY='KiEVTEhfZhZamuaQ3Hj2TjjHYYAkASrDbgxXT9f0'
 
 # Create your views here.
 
 def index(request):
     title = "RED-index"
-    return render(request,'RED/index.html', {'title': title})
+    # KiEVTEhfZhZamuaQ3Hj2TjjHYYAkASrDbgxXT9f0
+    today = date.today()
+    #today = today - timedelta(days=1)
+    previous_date = today - timedelta(days=1)
+    print(today)
+    print(previous_date)
+
+    nasa_api_response = nasa_api.get_data(NasaApI_KEY, today)
+    print("todays --",type(nasa_api_response))
+
+    previous_nasa_api_response = nasa_api.get_data(NasaApI_KEY, previous_date)
+    print("\nprevious days --",previous_nasa_api_response['url'])
+    jsondata = json.dumps(previous_nasa_api_response)
+    print(jsondata)
+
+
+    im_url = nasa_api.get_url(nasa_api_response)
+    im_url_previous = nasa_api.get_url(previous_nasa_api_response)
+
+    present_date = nasa_api.get_date(nasa_api_response)
+    yesterday = nasa_api.get_date(previous_nasa_api_response)
+
+    nasa_api.download_image(im_url, present_date)
+    nasa_api.download_image(im_url_previous, yesterday)
+    return render(request,'RED/index.html', {'title': title,'present_date':present_date, 'yesterday':yesterday, 'nasa_api':nasa_api ,
+        'previous_nasa_api_response':previous_nasa_api_response,'nasa_api_response':jsondata})
+
+
+# def index():
+#     # KiEVTEhfZhZamuaQ3Hj2TjjHYYAkASrDbgxXT9f0
+#     today = date.today()
+#     previous_date = today - timedelta(days=1)
+#     print(today)
+#     print(previous_date)
+
+#     nasa_api_response = nasa_api.get_data(SECRET_KEY, today)
+
+#     previous_nasa_api_response = nasa_api.get_data(SECRET_KEY, previous_date)
+
+#     im_url = nasa_api.get_url(nasa_api_response)
+#     im_url_previous = nasa_api.get_url(previous_nasa_api_response)
+
+#     present_date = nasa_api.get_date(nasa_api_response)
+#     yesterday = nasa_api.get_date(previous_nasa_api_response)
+
+#     nasa_api.download_image(im_url, present_date)
+#     nasa_api.download_image(im_url_previous, yesterday)
+
+#     return render_template("index.html", present_date=present_date, yesterday=yesterday, nasa_api=nasa_api, previous_nasa_api_response=previous_nasa_api_response,nasa_api_response=nasa_api_response)
+
+
+
+
+
+
+
 
 #@login_required
-def red_home(request):
+def home(request):
     title = "RED-home"
-    return HttpResponse('RED/home.html', {'title': title})
+    return render(request,'RED/home.html', {'title': title})
 
 def donate(request):
     title = "RED-donate"
-    return HttpResponse('RED/donate.html', {'title': title})
+    return render(request,'RED/donate.html', {'title': title})
 
 def history(request):
     title = "RED-history"
     return render(request,'RED/history.html', {'title': title})
+
 def haddits(request):
     title = "RED-haddits"
-    return HttpResponse('RED/haddits.html', {'title': title})
+    return render(request,'RED/haddit.html', {'title': title})
 
 def quran(request):
     title = "RED-quran"
@@ -34,7 +95,7 @@ def quran(request):
 
 def science(request):
     title = "RED-science"
-    return HttpResponse('RED/science.html', {'title': title})
+    return render(request,'RED/science.html', {'title': title})
 
 def local(request):
     title = "Local"
