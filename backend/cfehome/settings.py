0db557b0f0bccf2dc98e89360d42a7a082b386ec
Bashar12345 <abul35-2002@diu.edu.bd>
2022-05-12 12:01:08 +0600

    init

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-#print(BASE_DIR)
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,10 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 #print(STATIC_URL)
+#
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
